520135006dd9399b5c4f09aad451e1814aae9d01
Kona Marie <ambermoreyra@gmail.com>
2019-09-12 13:00:07 -0600

    Updates

---
+++ precache-manifest.a186a57e0627afcd78c2f99c256632a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1acf693de16bf6918efe6c98abf3324f",
+    "url": "/clickGame/index.html"
+  },
+  {
+    "revision": "182bfbbb4fb81e736d26",
+    "url": "/clickGame/static/css/main.ddbf7046.chunk.css"
+  },
+  {
+    "revision": "c5b1ec8be5540a9e641e",
+    "url": "/clickGame/static/js/2.75122b9b.chunk.js"
+  },
+  {
+    "revision": "182bfbbb4fb81e736d26",
+    "url": "/clickGame/static/js/main.1d945e85.chunk.js"
+  },
+  {
+    "revision": "4739d798b219ef4ef8e0",
+    "url": "/clickGame/static/js/runtime~main.076932a1.js"
+  }
+]);
