d63008e9c890db7958d1acff86c3aa255bc945b6
Kona Marie <ambermoreyra@gmail.com>
2019-09-16 13:45:24 -0600

    updated styling and completed README

--- clickgame/src/components/Header/Header.js
+++ clickgame/src/components/Header/Header.js
@@ -6,6 +6,12 @@ const Header = props => (
    <div className="scores">
      Score: {props.score} Highscore: {props.highscore}
    </div>
+   <br>
+   </br>
+   <br>
+   </br>
+   <div className="description">Click on an image to earn points, but don't click on any more than once!</div>
+
  </div>
 );
 export default Header;
